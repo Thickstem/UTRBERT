f4373d4971092a35ca16cc70665bba2d9293e38a
Thickstem <mosohaso3560@gmail.com>
2023-01-25 14:19:44 +0800

    mernize

--- Build_enst_dataset/preprocessing.py
+++ Build_enst_dataset/preprocessing.py
@@ -14,6 +14,14 @@ def _argparse():
     return args
 
 
+def seq2mer(seq, kmer=3):
+    mers = []
+    for i in range(len(seq) - kmer + 1):
+        mers.append(seq[i : i + kmer])
+    mers = " ".join(mers)
+    return mers
+
+
 def match(seq_db: pd.DataFrame, TE_data: pd.DataFrame):
     seq_db_ids = seq_db["trans_id"].values
     TE_data_ids = TE_data["ensembl_tx_id"].values
@@ -27,6 +35,13 @@ def match(seq_db: pd.DataFrame, TE_data: pd.DataFrame):
     return seq_db
 
 
+def mernize(matched_db):
+    matched_db["fiveprime"] = list(map(seq2mer, matched_db["fiveprime"].values))
+    matched_db["threeprime"] = list(map(seq2mer, matched_db["threeprime"].values))
+    matched_db["cds"] = list(map(seq2mer, matched_db["cds"].values))
+    return matched_db
+
+
 if __name__ == "__main__":
     args = _argparse()
 
@@ -35,4 +50,5 @@ if __name__ == "__main__":
     TE_data = TE_data[TE_data.isnull().sum(axis=1) == 0]
 
     matched_df = match(seq_db, TE_data)
+    matched_df = mernize(matched_df)
     matched_df.to_csv(os.path.join("../data/", args.save))
